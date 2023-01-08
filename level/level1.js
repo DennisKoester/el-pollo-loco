737c088dc11819deb6335983a913fcdcf5f09240
StayAbroad <dennis.koester11@gmail.com>
2023-01-08 23:47:32 +0100

    second cloud

--- level/level1.js
+++ level/level1.js
@@ -3,11 +3,16 @@ const level1 = new Level(
         new Chicken(),
         new Chicken(),
         new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
         new Endboss()
     ],
 
     [
-        new Cloud()
+        new Cloud('../img/5_background/layers/4_clouds/1.png'),
+        new Cloud('../img/5_background/layers/4_clouds/2.png'),
     ],
 
     [
--- models/cloud.class.js
+++ models/cloud.class.js
@@ -3,17 +3,20 @@ class Cloud extends MoveableObject {
     height = 300;
     width = 500;
 
-    constructor() {
-        super().loadImage('../img/5_background/layers/4_clouds/1.png');
+    constructor(imagePath) {
+        super().loadImage(imagePath);
 
         this.x = Math.random() * 500;
+
+        this.speed = 0.15 + Math.random() * 0.5; // Math Random generates a number between 0 and 1
+
         this.animate();
     }
 
+
     animate() {
-        this.moveLeft();
+        setInterval(() => {
+            this.moveLeft();
+        }, 1000 / 60);
     }
-
-
-
 }
