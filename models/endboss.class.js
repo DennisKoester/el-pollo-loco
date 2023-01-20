930d127d14110768e6053146048912193ed4bea7
StayAbroad <dennis.koester11@gmail.com>
2023-01-20 01:36:13 +0100

    endboss moveLeft fixed

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -5,6 +5,7 @@ class Endboss extends MoveableObject {
     x = 3800;
     hadFirstContact = false;
     speedThroughHit = 50;
+    speed = 10;
 
     offset = {
         top: 90,
@@ -14,14 +15,10 @@ class Endboss extends MoveableObject {
     }
 
     IMAGES_WALKING = [
-        './img/4_enemie_boss_chicken/2_alert/G5.png',
-        './img/4_enemie_boss_chicken/2_alert/G6.png',
-        './img/4_enemie_boss_chicken/2_alert/G7.png',
-        './img/4_enemie_boss_chicken/2_alert/G8.png',
-        './img/4_enemie_boss_chicken/2_alert/G9.png',
-        './img/4_enemie_boss_chicken/2_alert/G10.png',
-        './img/4_enemie_boss_chicken/2_alert/G11.png',
-        './img/4_enemie_boss_chicken/2_alert/G12.png',
+        './img/4_enemie_boss_chicken/1_walk/G1.png',
+        './img/4_enemie_boss_chicken/1_walk/G2.png',
+        './img/4_enemie_boss_chicken/1_walk/G3.png',
+        './img/4_enemie_boss_chicken/1_walk/G4.png'
     ];
 
     IMAGES_ALERT = [
@@ -84,7 +81,7 @@ class Endboss extends MoveableObject {
 
     animateEndboss() {
         if (!this.isDead()) {
-            this.playAnimation(this.IMAGES_ALERT);
+            this.playAnimation(this.IMAGES_WALKING);
             this.moveLeft();
             console.log('Endboss alive');
 
